45d70564059e42509e36e99926331dfd6d9d0496
mmwathe <mmwathe@andrew.cmu.edu>
2024-03-14 16:28:57 -0400

    primes

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -75,7 +75,7 @@ export default function QueryProcessor(query: string): string {
       const isCube = Number.isInteger(cubeRoot);
 
       if (isSquare && isCube) {
-        return (num.toString());
+        return num.toString();
       }
     }
 
@@ -95,7 +95,46 @@ export default function QueryProcessor(query: string): string {
       return "The array is empty";
     }
 
-    return ((num_list[0]*num_list[1]).toString());
+    return (num_list[0] * num_list[1]).toString();
+  }
+
+  function isPrime(number: number) {
+    // Check if the number is less than 2, as numbers less than 2 are not prime
+    if (number < 2) {
+        return false;
+    }
+
+    // Check for divisibility by numbers up to the square root of the given number
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        // If the number is divisible by any number other than 1 and itself, it's not prime
+        if (number % i === 0) {
+            return false;
+            break; // This line can be added to explicitly break out of the loop
+        }
+    }
+
+    // If no divisors other than 1 and itself are found, the number is prime
+    return true;
+  }
+
+  if (query.toLowerCase().includes("primes")) {
+    const numbers = query.match(/\d+/g);
+    const num_list = numbers?.map((number) => parseInt(number));
+    if (num_list === undefined) {
+      return "The array is undefined";
+    }
+
+    if (num_list.length === 0) {
+      return "The array is empty";
+    }
+    let result= []
+    for (let i = 0; i < num_list.length; i++) {
+      if (isPrime(num_list[i])) {
+        result.push(num_list[i])
+      }
+    }
+
+    return result.join(", ");
   }
 
   return "";
